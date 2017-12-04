c6b432547c34f9ca19787cfe31ce3d2c1350e4cc
fabao167 <faad2005@hotmail.com>
2017-12-04 12:24:36 -0200

    Atualização em venda automatico
    atualização em venda automatico

--- src/Visao/ListVenda.java
+++ src/Visao/ListVenda.java
@@ -211,7 +211,7 @@ public class ListVenda extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        CadastroVendas cadVendas = new CadastroVendas();
+        CadVendaDialog cadVendas = new CadVendaDialog(this, true);
         cadVendas.setVisible(true);
         ((VendaTableModel) jTable1.getModel()).refresh();
     }//GEN-LAST:event_jButton1ActionPerformed
